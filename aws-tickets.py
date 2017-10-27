cbbe9d94f71259317d89c82cbed259c19aaa0546
eolson <eolson@clearslide.com>
2017-10-27 15:58:13 -0700

    change arguments and how the class is being used.

--- aws-tickets.py
+++ aws-tickets.py
@@ -7,20 +7,24 @@ if __name__ == '__main__':
     # Setup Arg parsing
     parser = argparse.ArgumentParser(description='AWS Event API scrapper')
     requiredNamed = parser.add_argument_group('Required Names Arguments')
-    requiredNamed.add_argument('-e', '--environment', help='Environment to pull AWS events from', required=True)
+    requiredNamed.add_argument('-s', '--service', help='Service to pull AWS events from', required=True)
+    requiredNamed.add_argument('-e', '--environment', help='Environment to pull AWS events from. If health '
+                                                           'services \'us-east-1\' is the only option', required=True)
     requiredNamed.add_argument('-i', '--instances', help='Instance or list of instances to pull events on',
-                               required=True)
+                               required=False)
     args = parser.parse_args()
 
     # Set env globally for all future AWS commands
-    aws = AWS(args.environment)
+    aws = AWS(args.service, env=args.environment)
 
     # Grab instance tags
-    instance_tags = args.instances.split(",")
+    if args.instances:
+        instance_tags = args.instances.split(",")
 
     # Grab any and all events
-    event = aws.get_aws_events(instance_tags)
+    event = aws.get_aws_event_status(filters={'eventStatusCodes': ['upcoming']})
 
     # Print what we find
     print(event)
     Utils.print_table(event)
+
